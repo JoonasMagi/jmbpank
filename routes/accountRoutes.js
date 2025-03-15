8da69642ccdec1ed4dd14490c53f8c53181210a4
JoonasMagi <joonas.magi@vikk.ee>
2025-03-15 11:52:56 +0200

    Update account routes to add user accounts endpoint

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const accountController = require('../controllers/accountController');
+const { authenticate } = require('../middleware/auth');
 
 // GET all accounts
 router.get('/', accountController.getAllAccounts);
+
+// GET accounts for authenticated user
+router.get('/user', authenticate, accountController.getUserAccounts);
 
 // GET account by account number
 router.get('/:accountNumber', accountController.getAccount);
 
 // POST create new account
-router.post('/', accountController.createAccount);
+router.post('/', authenticate, accountController.createAccount);
 
 module.exports = router;
